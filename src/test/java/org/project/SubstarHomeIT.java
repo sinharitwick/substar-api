74bd424402925d5643776421cb32e8e60f9fdb51
sinharitwick <sritwick10@gmail.com>
2025-08-24 23:51:20 +0530

    initial commit

---
+++ src/test/java/org/project/SubstarHomeIT.java
@@ -0,0 +1,8 @@
+package org.project;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class SubstarHomeIT extends SubstarHomeTest {
+    // Execute the same tests but in packaged mode.
+}
